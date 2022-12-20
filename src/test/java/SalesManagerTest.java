0f9e387f37cf58e725ff51cc73f32ad5eecddee9
Egor Laptev <laptencio@yandex.ru>
2022-12-20 15:35:26 +0400

    test and maven added

---
+++ src/test/java/SalesManagerTest.java
@@ -0,0 +1,34 @@
+import com.kowalski.SalesManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SalesManagerTest {
+
+SalesManager salesManager = new SalesManager(new long[]{3,5,12,34,65,48,37,9,26});
+
+    @Test
+    public void testConstructor() {
+        SalesManager salesManager1 = new SalesManager(new long[]{3, 7, 9});
+        Assertions.assertNotNull(salesManager1);
+    }
+
+    @Test
+    public void testMaxFunction(){
+        double res = salesManager.max();
+        Assertions.assertEquals(65,res);
+    }
+
+    @Test
+    public void testTrimFunction(){
+        double res = salesManager.trim();
+        Assertions.assertEquals(30,res);
+    }
+
+    @Test
+    public void testTrimDivByZeroFunction() {
+        SalesManager salesManager2 = new SalesManager(new long[]{1,1});
+        Assertions.assertThrows(ArithmeticException.class, () ->  salesManager2.trim());
+    }
+}
+
+
